b2b04dbfb2938bf526f3cca3ceb615673c1ea2a4
jorge cohen <jorgel-79@hotmail.com>
2019-10-02 20:52:09 -0500

    version ionic prueba 2 de octubre 2

---
+++ prueba2ionic/src/app/tab2/tab2.page.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  constructor() {}
+
+  users: any[] = [
+    {
+      id: 1,
+      first: 'Alice',
+      last: 'Smith',
+    },
+    {
+      id: 2,
+      first: 'Bob',
+      last: 'Davis',
+    },
+    {
+      id: 3,
+      first: 'Charlie',
+      last: 'Rosenburg',
+    }
+  ];
+
+
+
+
+}
